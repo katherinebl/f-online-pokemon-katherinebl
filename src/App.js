844855888799a7709d6e43a05217bbf36e9fe89c
Katherine Briceño <kath.gbl@gmail.com>
2019-03-07 23:03:13 +0100

    create petition function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const ENDPOINT = "https://pokeapi.co/api/v2/pokemon/";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    };
+
+    this.getPokemonList();
+  }
+
+  getPokemonList() {
+    fetch(ENDPOINT)
+      .then(response => response.json())
+      .then(data => {
+        console.log("hola", data);
+      });
+    console.log("hola2");
+  }
+
   render() {
-    return <div className="App">:D</div>;
+    return (
+      <div className="app">
+        <main className="app__main">
+          <div className="app__main-wrapper">
+            <ul className="app__main-list" />
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
